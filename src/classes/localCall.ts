cfff435ada0ce5215ca473d0a554cd83b642b1e3
LautaroLovato <lautaro.lovato07@gmail.com>
2026-05-13 12:30:27 -0300

    refactor localCall

--- src/classes/localCall.ts
+++ src/classes/localCall.ts
@@ -3,16 +3,20 @@ import { Call } from './call';
 export class LocalCall extends Call {
     
     calculateCost(): number {
-        const dayOfWeek = this.date.getDay();
-        const hour = this.date.getHours();
-        
-        const isWeekend = dayOfWeek === 0 || dayOfWeek === 6;
-        const isBusinessHour = hour >= 8 && hour < 20;
-
-        if (isWeekend || !isBusinessHour) {
+        if (this.isWeekend() || !this.isBusinessHour()) {
             return this.durationInMinutes * 0.10;
         }
 
-        return this.durationInMinutes * 0.20;     
+        return this.durationInMinutes * 0.20;
+    }
+
+    private isWeekend(): boolean {
+        const day = this.date.getDay();
+        return day === 0 || day === 6; // 0 is Sunday, 6 is Saturday
+    }
+
+    private isBusinessHour(): boolean {
+        const hour = this.date.getHours();
+        return hour >= 8 && hour < 20;
     }
 }
